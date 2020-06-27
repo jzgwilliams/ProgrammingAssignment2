482e5e8943e02487cabe2ef37c84c51fa687ce77
jzgwilliams <66190875+jzgwilliams@users.noreply.github.com>
2020-06-27 17:34:34 -0400

    Added functions for assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+##The makeCacheMatrix function creates a Lexical Scope of parent,
+##children functions to return the special matrix, then provide
+##the inverse of the special matrix.  
 
-## Write a short comment describing this function
+##The makeCacheMatrix function is creating an open matrix "x"
+##where the number of rows and columns will be assigned at a later
+##time.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = numeric()) {
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(x) inv <<- solve(x)
+  getinv <- function() inv
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
-
-## Write a short comment describing this function
+##The cacheSolve function is designed to return the inverse of
+##the matrix "x" assigned in makeCacheMatrix.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv_result<- x$getinv()
+  if (!is.null(inv_result)){
+    message("getting cached data")
+    return(inv_result)
+  }
+  data <- x$get()
+  inv_result <- solve(data)
+  x$setinv(inv_result)
+  inv_result
 }
